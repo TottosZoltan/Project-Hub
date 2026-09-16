a81020dc558052ba923044a43c7bdbc661a9d8b8
TZ <Tottoszoltan01@gmail.com>
2026-09-16 07:26:41 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 const FRONTEND_URL =
-    "https://tottoszoltan.github.io/Project-Hub";
+    "https://tottoszoltan.github.io";
 
 const BACKEND_URL =
     "https://project-hub-backend-1.onrender.com";
