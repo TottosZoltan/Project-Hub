232f73493710ac5d5261f7b58fb865a0d3d30845
TZ <Tottoszoltan01@gmail.com>
2026-09-18 23:35:28 +0200

    Add files via upload

--- backend/config.js
+++ backend/config.js
@@ -3,6 +3,9 @@ const PORT = process.env.PORT || 10000;
 const FRONTEND_URL =
     "https://tottoszoltan.github.io";
 
+const PROJECT_HUB_URL =
+    "https://tottoszoltan.github.io/Project-Hub";
+
 const BACKEND_URL =
     "https://project-hub-backend-1.onrender.com";
 
@@ -12,6 +15,7 @@ const STEAM_API_KEY =
 module.exports = {
     PORT,
     FRONTEND_URL,
+    PROJECT_HUB_URL,
     BACKEND_URL,
     STEAM_API_KEY
 };
