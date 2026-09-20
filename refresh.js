00e00b49bc3dc5a3ef9f8fb74880b3a7ea921664
TZ <Tottoszoltan01@gmail.com>
2026-09-20 22:40:23 +0200

    bump app version to 1.5.2

--- refresh.js
+++ refresh.js
@@ -5,7 +5,7 @@
 (function () {
     "use strict";
 
-    const APP_VERSION = "1.5.1";
+    const APP_VERSION = "1.5.2";
     const PULL_THRESHOLD = 72;
     const EDGE_SWIPE_THRESHOLD = 55;
     const EDGE_ZONE = 28;
