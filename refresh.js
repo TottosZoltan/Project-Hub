eaa6d074101e8c293db09e94b030cace225c2848
TZ <Tottoszoltan01@gmail.com>
2026-09-22 21:49:31 +0200

    Sync version to 1.6.11

--- refresh.js
+++ refresh.js
@@ -5,7 +5,7 @@
 (function () {
     "use strict";
 
-    const APP_VERSION = "1.6.10";
+    const APP_VERSION = "1.6.11";
     const PULL_THRESHOLD = 72;
     const EDGE_SWIPE_THRESHOLD = 55;
     const EDGE_ZONE = 28;
